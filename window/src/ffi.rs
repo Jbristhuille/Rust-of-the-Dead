15b65c136e511f1f9e5812160f00973a995d1205
Jbristhuille <jbristhuille@gmail.com>
2025-06-19 21:10:37 +0200

    Add FFI binding for RegisterClassW in ffi.rs
    Introduces a new ffi.rs file with an FFI binding to the RegisterClassW function from user32, allowing registration of window classes using WNDCLASSW.

---
+++ window/src/ffi.rs
@@ -0,0 +1,15 @@
+/*****************************************************************************
+ * @Author                : Jbristhuille<jbristhuille@gmail.com>             *
+ * @CreatedDate           : 2025-06-19 21:09:37                              *
+ * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>             *
+ * @LastEditDate          : 2025-06-19 21:10:26                              *
+ ****************************************************************************/
+
+use crate::winapi_types::WNDCLASSW;
+use std::ffi::c_void;
+
+#[link(name = "user32")]
+extern "system" {
+    /// Registers a window class for subsequent use in calls to CreateWindow or CreateWindowEx.
+    pub fn RegisterClassW(lpWndClass: *const WNDCLASSW) -> u16;
+}
